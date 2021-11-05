86842bb451490764b2cb1b4d6242be783f2d397b
Captblaze <standbymii@yahoo.com>
2021-11-05 16:25:33 +0100

    Online Store Project

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $(".Ferrari").click(function(){
+        window.location.href="form.html";
+    });
+    
+    $(".Bugatti").click(function(){
+        window.location.href="form.html";
+    });
+    
+    $(".Mercedes").click(function(){
+        window.location.href="form.html";
+    });
+    
+    $(".Toyota").click(function(){
+        window.location.href="form.html";
+    });
+    
+    $(".Volvo").click(function(){
+        window.location.href="form.html";
+    });
+    
+    $(".Aston").click(function(){
+        window.location.href="form.html";
+    });
+    
+     $(".Porsche").click(function(){
+        window.location.href="form.html";
+    });
+    
+    $(".BMW").click(function(){
+        window.location.href="form.html";
+    });
+    
+    $(".Audi").click(function(){
+        window.location.href="form.html";
+    });
+    
+    $(".Honda").click(function(){
+        window.location.href="form.html";
+    });
+    
+    $(".Lexus").click(function(){
+        window.location.href="form.html";
+    });
+
+    $(".Maseratti").click(function(){
+        window.location.href="form.html";
+    });
+});
